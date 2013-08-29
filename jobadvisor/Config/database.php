5972af5ad2a6a1dfc7bcdde875de2c8557516066
Derek Hall <einhander.iniq@gmail.com>
2013-08-29 13:30:11 -0400

    Add DebugKit (multiple times, oops) and bake models/controllers/views

--- jobadvisor/Config/database.php
+++ jobadvisor/Config/database.php
@@ -61,21 +61,21 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'jobadvisor',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 
 	public $test = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'jobadvisor_test',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 }
