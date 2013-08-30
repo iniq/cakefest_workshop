9a2ba28cb6982e6314652ee637dae3fa87d09bfd
Derek Hall <einhander.iniq@gmail.com>
2013-08-30 13:56:28 -0400

    Set up some test basics

--- jobadvisor/Test/Case/Model/CompanyTest.php
+++ jobadvisor/Test/Case/Model/CompanyTest.php
@@ -14,7 +14,11 @@ class CompanyTest extends CakeTestCase {
  */
 	public $fixtures = array(
 		'app.company',
-		'app.job'
+		'app.job',
+		'app.cv',
+		'app.education',
+		'app.experiences',
+		'app.cvs_jobs',
 	);
 
 /**
@@ -38,4 +42,20 @@ class CompanyTest extends CakeTestCase {
 		parent::tearDown();
 	}
 
+	public function testGetRelatedJobs() {
+		$companyId = 1;
+
+		// are there 2 jobs?
+		$jobs = $this->Company->getRelatedJobs($companyId);
+		$this->assertEquals(2, count($jobs));
+
+		// do the jobs belong to company ID = $companyId?
+		$companyIdExtracted = Hash::extract($jobs, '{n}.Job.company_id');	// wtf is Hash::extract?
+		$this->assertEquals(1, count(array_unique($companyIdExtracted)));
+
+		// are they in the right order?
+		$jobIdExtracted = Hash::extract($jobs, '{n}.Job.id');
+		$expectedIdOrder = array(1, 2);
+		$this->assertEquals($expectedIdOrder, $jobIdExtracted, 'Jobs are not ordered by date');
+	}
 }
--- jobadvisor/Test/Fixture/JobFixture.php
+++ jobadvisor/Test/Fixture/JobFixture.php
@@ -37,6 +37,13 @@ class JobFixture extends CakeTestFixture {
 			'company_id' => 1,
 			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.'
 		),
+			'id' => 2,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created' => '2013-08-29 17:26:51',
+			'modified' => '2013-08-29 17:26:51',
+			'company_id' => 1,
+			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.'
+		),
 	);
 
 }
